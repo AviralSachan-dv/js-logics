d159b4e6007851c6275cbeb6834373d3d96e35d6
AviralSachan-dv <sachanaviral69@gmail.com>
2025-01-13 12:10:00 +0530

    Your commit message here

--- 10-01-2025/main.js
+++ 10-01-2025/main.js
@@ -1,17 +1,88 @@
-const data = [
-    { name: "ram", age: 21, state: "MP" },
-    { name: "shyam", age: 18, state: "MP" },
-    { name: "hera lal", age: 28, state: "UP" },
-];
+// to get dynamic data from Object data , when the value of state and age is changeable
 
-const state ="MP";
-const age = 22;
+// const data = [
+//     { name: "ram", age: 21, state: "MP" },
+//     { name: "shyam", age: 18, state: "MP" },
+//     { name: "hera lal", age: 28, state: "UP" },
+// ];
 
-const result = data.filter((item) => {
-    if (item.age >= age && item.state === state|| state === "") {
-        return true;
-    }
+// const state ="MP";
+// const age = 22;
+
+// const result = data.filter((item) => {
+//     if (item.age >= age && item.state === state|| !state) {
+//         return true;
+//     }
     
-});
+// });
 
-console.log(result);
+// console.log(result);
+ 
+
+
+
+// how to remove duplicate values from an array
+// const array = [99,1, 2, 2, 4, 1, 1, 4, 5, 6,99];
+
+// const uniqueArray = [...new Set(array)];
+// console.log(uniqueArray);
+
+
+//remove duplicate array using foreach  loop
+// const array = [99,1, 2, 2, 4, 1, 1, 4, 5, 6,99];
+
+
+// let uniqueArray = [];
+
+// array.forEach(item => {
+//   if (!uniqueArray.includes (item)) {
+//     uniqueArray.push(item);
+//   } 
+// });
+
+// console.log(uniqueArray); 
+
+// Using filter with indexOf to find the repeated elements
+//a1 = array.filter((item, index) => a.indexOf(item) === index);
+
+
+//counting vovels in  an array
+
+
+
+
+    
+        
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
